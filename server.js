6e018200da7fc45c801e089d23a369b15f23d155
Pruthviraj <gaikwadpruthviraj01@gmail.com>
2026-02-17 19:04:38 +0530

    Fixed port for Render deployment

--- server.js
+++ server.js
@@ -1,33 +1,21 @@
 const express = require('express');
 const path = require('path');
-<<<<<<< HEAD
 const fs = require('fs');
 const multer = require('multer');
 const { mergerPdfs } = require('./merge');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
-const port = 3000;
+const PORT = process.env.port || 3000;
 
 app.use('/static', express.static('public'));
 app.use(express.json());
 
 // Home
-=======
-const app = express();
-const multer = require('multer');
-const {mergerPdfs} = require('./merge') 
-const upload = multer({ dest: 'uploads/' });
-app.use('/static', express.static('public'))
-const port = 3000;
-
-// Serve the HTML form
->>>>>>> c23e2a308d4eb592a7312b69883385c76737918d
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'templates/index.html'));
 });
 
-<<<<<<< HEAD
 // Merge PDFs
 app.post('/merge', upload.array('pdfs'), async (req, res) => {
   try {
@@ -74,20 +62,6 @@ app.delete('/delete/:file', (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
-=======
-// Handle file uploads
-app.post('/merge', upload.array('pdfs', 2), async (req, res,) => {
-  //console.log(req.files); // Log uploaded files
-  let d = await mergerPdfs(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path))
-  res.redirect(`http://localhost:3000/static/${d}.pdf`)
-  // res.send({ data: req.files }); // Send response back to client
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
->>>>>>> c23e2a308d4eb592a7312b69883385c76737918d
