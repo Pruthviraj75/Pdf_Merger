8d652dd5f36f0b8148ad9fd88ee08bc12690bb0b
Pruthviraj <gaikwadpruthviraj01@gmail.com>
2026-02-17 19:49:19 +0530

    Clean setup for fresh deployment

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
 # File_Rename-DesktopApp
-=======
-# 📄 Pdf Merger - Using JS and Bootstrap
-
-This project is a **PDF Merger Web App** built using **HTML, CSS, JavaScript**, and styled with **Bootstrap**. It allows users to upload multiple PDF files and merge them into a single PDF file.
-
-## 🚀 Features
-- Upload multiple PDF files
-- Merge PDFs into a single file
-- Download the merged PDF
-- Responsive design using **Bootstrap**
-- User-friendly interface
->>>>>>> c23e2a308d4eb592a7312b69883385c76737918d
--- merge.js
+++ merge.js
@@ -1,6 +1,5 @@
 const PDFMerger = require('pdf-merger-js');
 
-<<<<<<< HEAD
 const mergerPdfs = async (files) => {
   const merger = new PDFMerger();
 
@@ -15,16 +14,3 @@ const mergerPdfs = async (files) => {
 };
 
 module.exports = { mergerPdfs };
-=======
-var merger = new PDFMerger();
-
-const mergerPdfs = async (p1, p2) => {
-  await merger.add(p1);  //merge all pages. parameter is the path to file and filename.
-  await merger.add(p2); // merge only page 2
-  let d = new Date().getTime();
-  await merger.save(`public/${d}.pdf`); //save under given name and reset the internal document
-  return d
-};
-
-module.exports = {mergerPdfs};
->>>>>>> c23e2a308d4eb592a7312b69883385c76737918d
